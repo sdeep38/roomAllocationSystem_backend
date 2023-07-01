993120f78bd12458405a1e03d8a25dcb4f3ce8ae
Soumyadeep <soumyadip243@gmail.com>
2023-07-01 22:13:42 +0530

    added new middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { changePassword, forgotPassword, login, logout, register, resetPassword } from "../controllers/auth.js"
+import { authMiddleware } from "../middlewares/auth.js"
 
 const router = express.Router()
 
@@ -8,6 +9,6 @@ router.post("/login", login)
 router.post("/logout", logout)
 router.post("/forgotPassword", forgotPassword)
 router.post("/resetPassword", resetPassword)
-router.post("/changePassword", changePassword)
+router.post("/changePassword", authMiddleware, changePassword)
 
 export default router;
