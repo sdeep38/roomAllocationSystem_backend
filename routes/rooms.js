a96ebd7a84bccc8ba7361ec0eadc72a6c30227d9
Soumyadeep <soumyadip243@gmail.com>
2025-12-24 21:10:42 +0530

    feat: added allocateroom, roomavailability, releaseroomlock routes and improved rooms.js paths

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,12 +1,16 @@
 import express from "express"
-import { addRooms, getRoom, getRooms, updateRoom } from "../controllers/rooms.js"
+import { addRooms, allocateRoom, allocateRoomController, checkRoomAvailability, getRoomByStudent, getRooms, releaseRoomLock, updateRoom } from "../controllers/rooms.js"
+import { authMiddleware } from "../middlewares/auth.js"
 
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/getRooms/:status', getRooms)
-router.get('/getRoom/:id', getRoom)
+router.get('/getRooms/:status', authMiddleware, getRooms)
+router.get('/getRoom/:student_id', getRoomByStudent)
 router.put('/updateRoom/', updateRoom)
 router.post('/addRooms', addRooms)
+router.put('/allocateRoom', allocateRoomController)
+router.post('/availability', authMiddleware, checkRoomAvailability)
+router.post('/lock/release', authMiddleware, releaseRoomLock)
 
 export default router;
