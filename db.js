a4e0fad92a8ba005f62130a1c80cf0b24380d627
Soumyadeep <soumyadip243@gmail.com>
2025-12-24 20:46:09 +0530

    refactor: updated db.js to use pool and mysql2/promise

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
-import mysql from "mysql2"
+import mysql from "mysql2/promise"
 
-export const db = mysql.createConnection({
+export const pool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "Mysql#123",
-    database: "test"
+    database: "test",
+    waitForConnections: true,
+    connectionLimit: 10,    // max simultaneous connections
+    queueLimit: 0,          // unlimited queued requests
 })
